e816d70fd6a1dec0cc250d5c0ad67203d426e9b7
Steven Perva <steven.perva@ensono.com>
2023-12-01 22:56:47 -0600

    Initial commit, waiting for challenge...

---
+++ day2/day2_pt1.py
@@ -0,0 +1,13 @@
+# *      /\    *         *
+#   *   //\\        *  
+#      //  \\           *     ***  | 
+#  *  //    \\   *            * *  |      S e v e n 9 s
+#    //      \\     *         ***  | Modern Mainframe Culture
+#  *//        \\                *  | 
+#  //  Day 2   \\ *           ***  | 
+# //____________\\   *
+#       ||||  *
+#   *   ||||     *    Seven9s.com - Buy a shirt or something
+
+with open('input', 'r', encoding='cp1047') as file:
+    print('Gotcha!')
