0dbd024650a9f8de4f54e43798287a55f14dcba5
Steven Perva <steven.perva@ensono.com>
2023-12-02 22:25:55 -0600

    Laying foundation for day 3...

---
+++ day3/day3_pt1.py
@@ -0,0 +1,24 @@
+# *      /\    *         *
+#   *   //\\        *  
+#      //  \\           *     ***  | 
+#  *  //    \\   *            * *  |      S e v e n 9 s
+#    //      \\     *         ***  | Modern Mainframe Culture
+#  *//        \\                *  | 
+#  //  Day 3   \\ *           ***  | 
+# //____________\\   *
+#       ||||  *
+#   *   ||||     *    Seven9s.com - Buy a shirt or something
+#--------------------------------------------------------------
+# Q: Why did Santa's helper see the doctor?
+# A: Because they had low 'elf' esteem.
+
+with open('input', 'r', encoding='cp1047') as file:
+    data = file.readlines()
+    
+    # Read the data and split it into records
+    for line in data:
+        # Strip any extra whitespace
+        line = line.strip()
+        
+        #
+        print(line)
