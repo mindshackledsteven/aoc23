c152eb062a1f184766881f154c72c75ba8aa9d3e
Steven Perva <steven.perva@ensono.com>
2023-12-03 22:48:36 -0600

    Prep for day 4.

---
+++ day4/day4_pt1.py
@@ -0,0 +1,19 @@
+# *      /\    *         *
+#   *   //\\        *  
+#      //  \\           *     ***  | 
+#  *  //    \\   *            * *  |      S e v e n 9 s
+#    //      \\     *         ***  | Modern Mainframe Culture
+#  *//        \\                *  | 
+#  //  Day 4   \\ *           ***  | 
+# //____________\\   *
+#       ||||  *
+#   *   ||||     *    Seven9s.com - Buy a shirt or something
+#--------------------------------------------------------------
+# Q: What do you call an elf who rhymes?
+# A: A wrapper
+
+# Read the input data
+with open('input', 'r', encoding='cp1047') as file:
+    data = file.readlines()
+    
+    print(data)
