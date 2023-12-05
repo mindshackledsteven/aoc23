1a9259f29cd8e0df2b0644724f6e0c78b8596cc3
Steven Perva <steven.perva@ensono.com>
2023-12-04 22:46:01 -0600

    Initial commit. Getting ready for day 5.

---
+++ day5/day5_pt.py
@@ -0,0 +1,19 @@
+# *      /\    *         *
+#   *   //\\        *  
+#      //  \\           *     ***  | 
+#  *  //    \\   *            * *  |      S e v e n 9 s
+#    //      \\     *         ***  | Modern Mainframe Culture
+#  *//        \\                *  | 
+#  //  Day 5   \\ *           ***  | 
+# //____________\\   *
+#       ||||  *
+#   *   ||||     *    Seven9s.com - Buy a shirt or something
+#--------------------------------------------------------------
+# Q: What do you give a Christmas tree with bad breath?
+# A: OrnaMINTS
+
+# Read the input data
+with open('input', 'r', encoding='cp1047') as file:
+    
+    for line in file.readlines():
+        print(line)
